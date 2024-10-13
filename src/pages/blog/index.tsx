f86eaaad3b2a88be0819082c85074bfd9dd4c39b
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2024-10-13 20:48:32 +0100

    blog edital

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,7 +1,14 @@
 import { ArrowLeft, Facebook, Instagram, Twitter, Youtube } from "lucide-react";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function BlogPage() {
+
+  const navigate = useNavigate() 
+
+  function homePage() {
+    navigate('/')
+  }
 
   	// Estado para o sroller
 	const [isScrolled, setIsScrolled] = useState(false);
@@ -32,22 +39,35 @@ export function BlogPage() {
 
           <div className="w-40 flex items-center justify-between">
             <div>
-              <ArrowLeft className="size-6" />
+              {/* biome-ignore lint/a11y/useButtonType: <explanation> */}
+              <button onClick={homePage}>
+                <ArrowLeft className="size-6 mt-1" />
+              </button>
             </div>
-            <p>
+            <p className="text-2xl font-normal">
               Sobre nós
             </p>
           </div>
           <div className="flex items-center justify-between">
             {/*Social*/}
-            <div className="flex gap-1.5">
+            <div className="flex gap-2 text-zinc-100">
               {/* biome-ignore lint/a11y/useValidAnchor: <explanation> */}
               <a href="#">
                 <Facebook className="size-7" />
               </a>
-              <Instagram className="size-7" />
-              <Twitter className="size-7" />
-              <Youtube className="size-7" />
+              {/* biome-ignore lint/a11y/useValidAnchor: <explanation> */}
+              <a href="#">
+                <Instagram className="size-7" />
+              </a>
+              {/* biome-ignore lint/a11y/useValidAnchor: <explanation> */}
+              <a href="#">
+                <Twitter className="size-7" />
+              </a>
+
+              {/* biome-ignore lint/a11y/useValidAnchor: <explanation> */}
+              <a href="#">
+                <Youtube className="size-7" />
+              </a>
 
             </div>
           </div>
@@ -59,6 +79,41 @@ export function BlogPage() {
           <div>
             <img className="size-cover h-full w-full bg-cover bg-center" src="/banner2.jpg" alt="" />
           </div>
+
+          <div className="flex items-center justify-center flex-wrap gap-2">
+
+            <div>
+              <div className="w-10/12 leading-loose flex flex-col gap-4 justify-center mx-auto">
+                <h1 className="text-5xl text-buttonColor py-2 mt-12 font-normal">Sobre nós</h1>
+                <p className="text-lg text-zinc-300">
+                  "Olá! Sou o Cunha Shombossi, a mente e o coração por trás da Geladaria Italala.
+                  Minha paixão por criar sabores únicos e oferecer momentos doces para as pessoas começou há muitos anos,
+                  quando descobri a magia que um simples sorvete pode trazer para o dia de alguém."
+                </p>
+                <p className="text-lg text-zinc-300">
+                  "Na Geladaria Itala, usamos apenas ingredientes frescos e de alta qualidade. Todos os nossos sorvetes são preparados com muito cuidado e amor, para garantir uma explosão de sabores em cada mordida. Valorizamos o processo artesanal, e cada detalhe é pensado para oferecer a melhor experiência aos nossos clientes."
+                </p>
+                <p className="text-lg text-zinc-300">
+                  "Na Italala, você encontrará um ambiente familiar e descontraído, perfeito para saborear um delicioso sorvete em boa companhia. Além dos nossos sabores tradicionais, oferecemos opções veganas e sem lactose, para que todos possam aproveitar essa experiência. Acompanhe nossas redes sociais e fique por dentro das nossas promoções e novidades, Visite-nos ja, ou  faça sua encomenda por aqui!"
+                </p>
+              </div>
+            </div>
+
+            <div>
+              <h1 className="text-5xl text-buttonColor py-2 mt-12 font-normal">Depoimentos</h1>
+              <figure className="">
+                <div>
+
+                </div>
+                <div>
+                  
+                </div>
+              </figure>
+            </div>
+
+
+          </div>
+
         </div>
 
 
