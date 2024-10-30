ee4382682baaa67f505b82b8f8984eeef712c0ca
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2024-10-30 07:48:18 +0100

    remove logos contact

--- src/components/contact-and-language/contact-and-language.tsx
+++ src/components/contact-and-language/contact-and-language.tsx
@@ -2,12 +2,16 @@ import { Headset, Languages } from "lucide-react";
 
 export function ContactAndLanguage() {
 	return (
-		<div className="flex h-14 justify-center cursor-pointer">
+		<div className="flex justify-center cursor-pointer">
 			{/** <img className="w-40" src="/logo-geladaria.png" alt="logoItalala" /> */}
 
-			<div className="flex items-center bg-headerColor text-buttonColor px-6 py-3 rounded-full w-96 justify-between font-medium text-lg">
-        <Headset />
-        <Languages />
+			<div className="flex gap-3 items-center bg-headerColor text-buttonColor rounded-full w-96 justify-between font-medium text-lg">
+        <div className="bg-white w-full rounded-full">
+					<Headset  />
+				</div>
+        <div className="bg-white w-full rounded-full">
+					<Languages  />
+				</div>
 			</div>
 		</div>
 	);
