f7717f0b4052ba57cc9fe129de8eb7aa67024d3f
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2024-10-30 14:36:45 +0100

    modal update

--- src/components/contact-and-language/contact-and-language.tsx
+++ src/components/contact-and-language/contact-and-language.tsx
@@ -5,7 +5,7 @@ export function ContactAndLanguage() {
 		<div className="flex justify-center cursor-pointer">
 			{/** <img className="w-40" src="/logo-geladaria.png" alt="logoItalala" /> */}
 
-			<div className="flex gap-2 items-center bg-searchColorInput text-buttonColor p-2.5 rounded-full w-96 justify-between font-medium text-lg">
+			<div className="flex gap-2 items-center bg-searchColor text-buttonColor p-2.5 rounded-full w-96 justify-between font-medium text-lg">
         <div className="bg-colorFundo w-full flex justify-between rounded-full py-2.5 px-5">
 					<p className="text-headerColor">Ligar</p>
 					<Headset  />
